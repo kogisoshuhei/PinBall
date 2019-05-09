4d97ce114e6e793ef94d1906e3c0eaf7a53c5ed5
shuhei kogiso <doodles9150@gmail.com>
2019-05-09 10:43:58 +0900

    PinBall 4回目のコミット
    Lesson5の発展課題の提出になります（タッチ処理の追加）

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -22,29 +22,64 @@ public class FripperController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
 
-		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+		//触れている指の本数分繰り返す
+		for (int i = 0; i < Input.touchCount; i++) {
+			
+			int id = Input.touches [i].fingerId;
+			Vector2 pos = Input.touches [i].position;
 
-			SetAngle (this.flickAngle);
+			//タッチ座標を表示
+			Debug.LogFormat ("{0} - x:{1}, y:{2}", id, pos.x, pos.y);
+
+			Touch touch = Input.GetTouch(id);
+
+			if ( touch.phase == TouchPhase.Began ) {
+				
+				if (pos.x > Screen.width * 0.5 && tag == "RightFripperTag") {
+
+					// 右フリッパーをあげる
+					SetAngle (this.flickAngle);
+
+				} else if (pos.x < Screen.width * 0.5 && tag == "LeftFripperTag") {
+
+					// 左フリッパーをあげる
+					SetAngle (this.flickAngle);
+
+				}
+
+			} else if ( touch.phase == TouchPhase.Ended ) {
+
+				if (tag == "RightFripperTag" || tag == "LeftFripperTag") {
+
+					// 右フリッパーを下げる
+					SetAngle (this.defaultAngle);
+
+				}
+			}
 		}
-
-		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
-
-			SetAngle (this.flickAngle);
-		}
-
-		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
-
-			SetAngle (this.defaultAngle);
-		}
-
-		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
-
-			SetAngle (this.defaultAngle);
-		}
-		
 	}
+
+
+//		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+//
+//			SetAngle (this.flickAngle);
+//		}
+//
+//		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+//
+//			SetAngle (this.flickAngle);
+//		}
+//
+//		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+//
+//			SetAngle (this.defaultAngle);
+//		}
+//
+//		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+//
+//			SetAngle (this.defaultAngle);
+//		}
 
 	public void SetAngle (float angle){
 
